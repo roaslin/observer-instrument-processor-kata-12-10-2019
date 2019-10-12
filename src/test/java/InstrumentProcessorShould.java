e695733dce67104be8de5c1e24aefa71b14461ba
roaslin <roaslin@gmail.com>
2019-10-12 20:18:43 +0200

    verify only that task is executed

--- src/test/java/InstrumentProcessorShould.java
+++ src/test/java/InstrumentProcessorShould.java
@@ -19,9 +19,9 @@ public class InstrumentProcessorShould {
 
 //   1- When the method Process is called then the InstrumentProcessor gets the next task from the
 //    task dispatcher  and executes it on the instrument.
-//   TODO
 //    2- When the Execute method of the instrument throws an exception then this exception is passed
 //    on to the caller of the Process method.
+//   TODO
 //    3- When the instrument fires the finished event then the InstrumentProcessor calls the task
 //    dispatcher’s FinishedTask method with the correct task.
 //    4- When the instrument fires the Error event then the InstrumentProcessor writes the string
@@ -47,7 +47,6 @@ public class InstrumentProcessorShould {
 
         instrumentProcessor.process();
 
-        verify(taskDispatcher, times(1)).getTask();
         verify(instrument, times(1)).execute(TASK);
     }
 
